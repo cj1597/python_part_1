aefa79b04d787ff76def8e9f997368f6d7b854f0
Clarence Joy Lapuz <clarencejoylapuz@gmail.com>
2021-08-14 21:19:39 +0800

    Modified Item 4

--- Item_4.py
+++ Item_4.py
@@ -3,7 +3,7 @@
 # int_list = [int(y) for y in nums]
 # cube_int = [x**3 for x in int_list]
 # print(cube_int)
-
+#
 
 given_int = input('Input a list of numbers: ')
 nums = list(given_int[1:-1].split(','))
